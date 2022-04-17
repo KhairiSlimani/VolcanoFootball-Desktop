bc8f0b0e6799a617e4f1207d25876d87bce3f268
Khairi Slimani <61380055+KhairiSlimani@users.noreply.github.com>
2022-04-17 18:02:24 +0100

    Interface Graphique

---
+++ src/Gui/user/RegisterController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui.user;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Khairi
+ */
+public class RegisterController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void Register(MouseEvent event) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    private void OpenLogin(MouseEvent event) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    
+}
---
+++ src/volcanofootballdesktop/Main.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volcanofootballdesktop;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Khairi
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../Gui/user/login.fxml"));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setScene(scene);
+        stage.show();    
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Produit p1 = new Produit("produit test 1","lunette","XL","Noir",5,"desc",10,"photo");
+        Produit p2 = new Produit("produit test 2","jackette","M","Blue",5,"desc",23,"photo");
+        ProduitService ps = new ProduitService();
+
+        ps.AjouterProduit(p1);
+        ps.AjouterProduit(p2);
+        ps.SupprimerProduit(13);
+        System.out.println(ps.AfficherProduits().toString());
+        
+        */
+        
+        launch(args);
+    }
+    
+}
